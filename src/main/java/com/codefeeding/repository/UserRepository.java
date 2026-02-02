608871e4d145d97d887631babf76fd16825dffa0
codefeeding99 <codefeeding@gmail.com>
2026-02-02 14:18:49 +0530

    Revert "my project files"
    This reverts commit 3b137581670bfef26ee3bba1228ac05e9815455e.

--- src/main/java/com/codefeeding/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codefeeding.repository;
-
-import com.codefeeding.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> { }
